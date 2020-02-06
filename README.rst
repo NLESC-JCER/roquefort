b007f138b2a6a44fa287daad3e80b222483fda27
felipez <tifonzafel@gmail.com>
2020-02-06 16:50:22 +0100

    first commit

---
+++ README.rst
@@ -0,0 +1,64 @@
+################################################################################
+refac
+################################################################################
+
+
+
+
+The project setup is documented in `a separate document <project_setup.rst>`_. Feel free to remove this document (and/or the link to this document) if you don't need it.
+
+Installation
+------------
+
+To install refac, do:
+
+.. code-block:: console
+
+  git clone https://github.com//refac.git
+  cd refac
+  pip install .
+
+
+Run tests (including coverage) with:
+
+.. code-block:: console
+
+  python setup.py test
+
+
+Documentation
+*************
+
+.. _README:
+
+Include a link to your project's full documentation here.
+
+Contributing
+************
+
+If you want to contribute to the development of refac,
+have a look at the `contribution guidelines <CONTRIBUTING.rst>`_.
+
+License
+*******
+
+Copyright (c) 2020, Netherlands eScience Center
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+
+Credits
+*******
+
+This package was created with `Cookiecutter <https://github.com/audreyr/cookiecutter>`_ and the `NLeSC/python-template <https://github.com/NLeSC/python-template>`_.
---
+++ refac/refactor.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python
+
+import argparse
+import re
+import string
+from itertools import chain, dropwhile, takewhile
+from pathlib import Path
+
+from pyparsing import (Char, Group, Literal, OneOrMore, Word, ZeroOrMore,
+                       alphanums)
+
+
+def parse_common_block(s: str) -> list:
+    """Parse a common block."""
+    myword = Word(alphanums + "_")
+    inside = OneOrMore(myword + ZeroOrMore(","))
+    parenthesis = ZeroOrMore(Char("(") + inside + Char(")"))
+    parser = Literal("common") + Char('/') + myword + Char('/') + \
+        OneOrMore(Group(myword + parenthesis + ZeroOrMore(Char(","))))
+
+    return parser.parseString(s).asList()
+
+
+class Refactor:
+    """Object to remove common block from a project."""
+
+    def __init__(self, name: str, path: Path):
+        """Initialize Refactor class."""
+        self.block_name = name
+        self.path = path
+        self.keyword = f"common /{name}/.*"
+
+    def has_common_block(self, path: Path) -> bool:
+        """Look up for a specific common block in a given file."""
+        with open(path, 'r') as f:
+            xs = f.read()
+
+        result = re.search(self.keyword, xs)
+        return True if result is not None else False
+
+    def generate_new_module(self, variables: list, variable_names: str) -> str:
+        """Generate new module replacing the common block."""
+        integer_variables = string.ascii_lowercase[8:14]
+
+        # Sort alphabetically
+        variables.sort()
+
+        def add_kind(v: str) -> str:
+            if v[0].lower() in integer_variables:
+                return "integer  ::"
+            else:
+                return "real(dp) ::"
+
+        kinds_and_variables = '\n'.join(
+            f"    {add_kind(v)} {v}" for v in variables)
+
+        new = f"""
+module {self.block_name}
+    !> Arguments: {variable_names}
+    use precision_kinds, only: dp
+    include 'vmc.h'
+
+{kinds_and_variables}
+
+    save
+end module {self.block_name}
+"""
+        return new
+
+    def get_target_files(self) -> tuple:
+        """Get the target files to replace."""
+        fortran_files = get_src_files(self.path, 'vmc')
+        includes = get_src_files(self.path, 'include')
+        target_source = list(
+            filter(lambda x: self.has_common_block(x), fortran_files))
+        target_include = list(
+            filter(lambda x: self.has_common_block(x), includes))
+
+        return target_source, target_include
+
+    def read_common_block_definition(self, path: Path) -> str:
+        """Read the definition of the common block."""
+        with open(path, 'r') as f:
+            xs = f.read()
+
+        # Get common block definition
+        result = re.search(self.keyword, xs)
+
+        # Continuation line
+        next_lines = result.string[result.end():].split()
+
+        # Search for continuation lines
+        new_line_index = result.end() + 6
+        start_common_block = result.group(0).strip()
+
+        # Search for first char in the newt line
+        if next_lines[0][0] != '&':
+            self.multiline = False
+            common_block = start_common_block
+        else:
+            self.multiline = True
+            common_block = start_common_block + \
+                search_for_line_continuation(result.string[new_line_index:])
+
+        return split_common_block(common_block)
+
+    def generate_module_call(self, definition: str) -> str:
+        """Generate the call to the new module and variable names."""
+        # remove parenthesis
+        variables = ', '.join(x.split('(')[0] for x in definition)
+        statement = f"use {self.block_name}, only: {variables}\n"
+
+        return statement, variables
+
+    def split_into_subroutines(self, path: Path):
+        """Split each file into its subroutines."""
+        def splitter(key: str) -> list:
+            return [key + x for x in xs.split(key) if x]
+        with open(path, 'r') as f:
+            xs = f.read()
+
+        # subroutine identifier
+        keyword_sub = "      subroutine "
+        keyword_fun = "      function"
+        if keyword_sub in xs:
+            return splitter(keyword_sub)
+        elif keyword_fun in xs:
+            return splitter(keyword_fun)
+        else:
+            raise RuntimeError("Neither subroutine or function found at {path}!")
+
+    def change_subroutine(self, module_call: str, xs: str) -> str:
+        """Replace common block in subroutine."""
+        # Search and removed implicit
+        before_implicit, after_implicit = split_str_at_keyword(
+            "implicit real.*", xs)
+        # search and removed common block
+        before_common, after_common = split_str_at_keyword(
+            self.keyword, after_implicit)
+
+        # if self.multiline:
+        #     # remaining = after_common[1:].split("\n")[:10]
+        #     xs = after_common[1:].split("\n")[:10]
+        #     print("xs: ", xs)
+        #     remaining = '\n'.join(
+        #         dropwhile(lambda x: x[5] == '&', xs))
+        #     print("remaining: ", remaining)
+        #     # after_common = remaining
+
+        new_subroutine = before_implicit + module_call + \
+            "      implicit real*8(a-h,o-z)\n" + before_common + after_common
+
+        return new_subroutine
+
+    def replace_common_blocks(self, module_call: str, files: list):
+        """Replace the common block for the subroutines in the file."""
+        def conditional_replacement(x: str) -> str:
+            return self.change_subroutine(module_call, x) if f"/{self.block_name}/" in x else x
+
+        for path in files:
+            print("Changing file: ", path)
+            new_subroutines = '\n'.join(conditional_replacement(
+                x) for x in self.split_into_subroutines(path))
+            with open(path, 'r+') as f:
+                f.write(new_subroutines)
+
+    def add_new_module(self, new_module: str):
+        """Add new module replacing the common block."""
+        with open(self.path / "src/vmc/m_common.f90", 'a') as f:
+            f.write(new_module)
+
+    def refactor(self):
+        """Remove common block."""
+        target_source, target_include = self.get_target_files()
+        if target_include:
+            print("Shit happens! There are common blocks in the include files")
+        elif not target_source:
+            print(
+                f"There is not {self.block_name} common block in the source files")
+        else:
+            definition = self.read_common_block_definition(target_source[0])
+            print("definition: ", definition)
+            module_call, variables = self.generate_module_call(definition)
+            new_module = self.generate_new_module(definition, variables)
+            self.replace_common_blocks(module_call, target_source)
+            self.add_new_module(new_module)
+
+
+def split_str_at_keyword(keyword: str, lines: str) -> str:
+    """Split lines at `keyword` returning the lines before and after keyword."""
+    result = re.search(keyword, lines)
+    sub = result.string
+    return sub[:result.start()], sub[result.end():]
+
+
+def split_common_block(s: str) -> list:
+    """Split common block into its individual variables."""
+    xs = parse_common_block(s)
+    # remove initial kewords
+    lists = xs[4:]
+
+    # comma separate variables
+    variables = [''.join(x) for x in lists]
+
+    # remove commas
+    return [x if x[-1] != ',' else x[:-1] for x in variables]
+
+
+def search_for_line_continuation(s: str) -> str:
+    """Search for & line continuations."""
+    xs = takewhile(lambda x: x.startswith("&"), s.split())
+
+    # Remove the & symbol
+    return ''.join([x[1:] for x in xs])
+
+
+def get_src_files(path: Path, folder: str):
+    """List of the fortran 77 source files."""
+    vmc_path = path / f"src/{folder}"
+    return chain(vmc_path.glob("*.f"), vmc_path.glob("*.h"))
+
+
+def main():
+    """Parse the command line arguments."""
+    parser = argparse.ArgumentParser(
+        description="refactor -n <common block name> -p folder_path")
+    # configure logger
+    parser.add_argument('-n', required=True,
+                        help="Common block name")
+    parser.add_argument('-p', help="path to champ", default=".")
+    args = parser.parse_args()
+
+    rs = Refactor(args.n, Path(args.p))
+    rs.refactor()
+
+
+if __name__ == "__main__":
+    main()
